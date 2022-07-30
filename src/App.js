6b067ca3fdae1ec5852ed5272fc6a70e65b77b7e
unknown <koo6250@naver.com>
2022-07-30 20:33:53 +0900

    feat login and sign

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Hello from "./components/Hello";
+import LoginPage from './pages/LoginPage';
+import JoinPage from './pages/JoinPage';
+import CMInput from './pages/components/CMInput';
+import CMButton from './pages/components/CMButton';
+import { BrowserView, MobileView } from 'react-device-detect'
+import JoinAdress from './pages/JoinAdress';
+import JoinMoney from './pages/JoinMoney';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ var container = {
+ backgroundColor: 'white',
+ width: '80%',
+ margin: '0 auto',
+ };
+ return (
+ <div style={container}>
+ <LoginPage />
+ <hr />
+ <JoinPage />
+ <hr />
+ <JoinAdress/>
+ <hr />
+ <JoinMoney/>
+
+ </div>
+ );
 }
+
+
 
 export default App;
