f7f556ae79f7c101f5c8f9f8422b5c712b993302
Yejin Shin <shinyejin0212@gmail.com>
2022-10-04 21:36:29 +0900

    detail 중

--- src/components/StoreMain.js
+++ src/components/StoreMain.js
@@ -2,7 +2,7 @@
 import { AiFillStar } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 
-const StoreMain = () => {
+const StoreMain = (id) => {
   const navigate = useNavigate();
   return (
     <>
@@ -10,69 +10,15 @@ const StoreMain = () => {
       <div className="StoreInfoImage" style={{ float: "left", margin: 10 }}>
         <img
           onClick={() => {
-            navigate("/store");
+            window.location.href = `/storelist/${id}`;
+            // navigate(`/storelist/${id}`);
+            // onClick={() => (window.location.hrf = `/storelist/${id}`)}
           }}
           className="storeImg"
-          src="img/storelogo.png"
+          src="./img/storelogo.png"
           width="100"
           height="100"
           alt="끝내주는 마라탕의 로고"
-        />
-      </div>
-      <div className="StoreMenuText" style={{ float: "left", margin: 10 }}>
-        <b>끝내주는 마라탕</b>
-        <br></br>
-        <AiFillStar />
-        <AiFillStar />
-        <AiFillStar />
-        <AiFillStar />
-        <AiFillStar />
-        {"     "}4.9 (59) <br></br>
-        최소 주문 금액 <b>12,000원</b>
-        <br></br>
-        배달 소요 시간 <b>52~62분</b>
-      </div>
-      <div style={{ clear: "both" }}></div>
-      <hr style={{ width: 500 }}></hr>
-      <div className="StoreInfoImage" style={{ float: "left", margin: 10 }}>
-        <img
-          onClick={() => {
-            navigate("/store");
-          }}
-          className="storeImg"
-          src="img/storelogo.png"
-          width="100"
-          height="100"
-          alt="끝내주는 마라탕의 로고"
-          align-items="left"
-        />
-      </div>
-      <div className="StoreMenuText" style={{ float: "left", margin: 10 }}>
-        <b>끝내주는 마라탕</b>
-        <br></br>
-        <AiFillStar />
-        <AiFillStar />
-        <AiFillStar />
-        <AiFillStar />
-        <AiFillStar />
-        {"     "}4.9 (59) <br></br>
-        최소 주문 금액 <b>12,000원</b>
-        <br></br>
-        배달 소요 시간 <b>52~62분</b>
-      </div>
-      <div style={{ clear: "both" }}></div>
-      <hr style={{ width: 500 }}></hr>
-      <div className="StoreInfoImage" style={{ float: "left", margin: 10 }}>
-        <img
-          onClick={() => {
-            navigate("/store");
-          }}
-          className="storeImg"
-          src="img/storelogo.png"
-          width="100"
-          height="100"
-          alt="끝내주는 마라탕의 로고"
-          align-items="left"
         />
       </div>
       <div className="StoreMenuText" style={{ float: "left", margin: 10 }}>
