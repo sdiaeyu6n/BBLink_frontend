9a6b64a113c79837673f72c29772019bd020af38
Yejin Shin <shinyejin0212@gmail.com>
2022-10-05 15:18:32 +0900

    mypage -> MyPage

--- src/pages/mypage.js
+++ src/pages/mypage.js
@@ -1,97 +1,57 @@
-import React from "react";
-import HeadButton from "../components/HeadButton";
-import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-const StyledButton2 = styled.button`
-  height: 60px;
-  width: 130px;
-  background-color: #ffef82;
-  font-size: 20px;
-  font-weight: 700;
-  font-color: black;
-`;
+import React from 'react';
+import HeadButton from '../components/HeadButton';
+import {useNavigate} from "react-router-dom";
 
-const StyledButton = styled.button`
-  height: 60px;
-  width: 200px;
-  background-color: #ffef82;
-  font-size: 20px;
-  font-weight: 700;
-  font-color: black;
-`;
 
-const StyledButton3 = styled.button`
-  height: 60px;
-  width: 300px;
-  background-color: #ffef82;
-  border-color: #ffef82;
-  font-size: 25px;
-  font-weight: 700;
-  border-radius: 50px;
-`;
+function MyPage() {
 
-export default function MyPage() {
-  const navigate = useNavigate();
+    const navigate=useNavigate();
 
-  return (
-    <div>
-      <center>
-        <br />
-        <HeadButton text={"마이페이지"} />
-        <br />
-        <br />
-        <StyledButton>귀여운뽀미 님</StyledButton>
-        &nbsp; &nbsp;
-        <StyledButton2>로그아웃</StyledButton2>
-        <br />
-        <br />
-        <StyledButton3
-          onClick={() => {
-            navigate("/PersonalInfo");
-          }}
-        >
-          개인정보 관리
-        </StyledButton3>
-        <br />
-        <br />
-        <StyledButton3
-          onClick={() => {
-            navigate("/AdressManage");
-          }}
-        >
-          주소 관리
-        </StyledButton3>
-        <br />
-        <br />
-        <StyledButton3
-          onClick={() => {
-            navigate("/ReviewManage");
-          }}
-        >
-          리뷰 관리
-        </StyledButton3>
-        <br />
-        <br />
-        <StyledButton3
-          onClick={() => {
-            navigate("/Settings");
-          }}
-        >
-          설정
-        </StyledButton3>
-        <br />
-        <br />
-        <StyledButton3
-          onClick={() => {
-            navigate("/Notice");
-          }}
-        >
-          공지사항
-        </StyledButton3>
-        <br />
-        <br />
-        <br />
-      </center>
-    </div>
-  );
+    return (
+
+    
+ <div>
+
+    <center>
+    
+ <h1>마이페이지 </h1> 
+ <HeadButton text={'닉네임'}/>
+<button>로그아웃</button>
+<br/>
+<br/>
+<button onClick={()=>{
+navigate('/PersonalInfo')
 }
+}>개인정보 관리</button>
+ <br/>
+ <br/>
+ <button onClick={()=>{
+navigate('/AdressManage')
+}
+}>주소 관리</button>
+ <br/>
+ <br/>
+ <button onClick={()=>{
+navigate('/ReviewManage')
+}
+}>리뷰 관리</button>
+ <br/>
+ <br/>
+ <button onClick={()=>{
+navigate('/Settings')
+}
+}>설정</button>
+ <br/>
+ <br/>
+ <button onClick={()=>{
+navigate('/Notice')
+}
+}>공지사항</button>
+ <br/>
+ <br/>
+ <br/>
+ </center>
+ </div>
+ );
+}
+export default MyPage;
