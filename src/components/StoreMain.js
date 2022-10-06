6967c1e131b36e07316dfa7b8abd95cb02905706
sdiaeyu6n <qdial214@dgu.ac.kr>
2022-10-06 19:58:01 +0900

    storelist 추가

--- src/components/StoreMain.js
+++ src/components/StoreMain.js
@@ -159,6 +159,161 @@ const StoreMain = (id) => {
         배달 소요 시간 <b>27분</b>
       </div>
       <div style={{ clear: "both" }}></div>
+
+      <hr style={{ width: 500 }}></hr>
+      <div className="StoreInfoImage" style={{ float: "left", margin: 10 }}>
+        <img
+          onClick={() => {
+            window.location.href = `/storelist/${id}`;
+            // navigate(`/storelist/${id}`);
+            // onClick={() => (window.location.hrf = `/storelist/${id}`)}
+          }}
+          className="storeImg"
+          src="./img/storelogo_신전떡볶이-대학로점.png"
+          width="100"
+          height="100"
+          alt="신전떡볶이 대학로점의 로고"
+        />
+      </div>
+      <div className="StoreMenuText" style={{ float: "left", margin: 10 }}>
+        <b>신전떡볶이-대학로점</b>
+        <br></br>
+        <AiFillStar />
+        {"     "}4.9 (485) <br></br>
+        최소 주문 금액 <b>13,000원</b>
+        <br></br>
+        배달 소요 시간 <b>30분</b>
+      </div>
+      <div style={{ clear: "both" }}></div>
+
+      <hr style={{ width: 500 }}></hr>
+      <div className="StoreInfoImage" style={{ float: "left", margin: 10 }}>
+        <img
+          onClick={() => {
+            window.location.href = `/storelist/${id}`;
+            // navigate(`/storelist/${id}`);
+            // onClick={() => (window.location.hrf = `/storelist/${id}`)}
+          }}
+          className="storeImg"
+          src="./img/storelogo_종점떡볶이.png"
+          width="100"
+          height="100"
+          alt="종점떡볶이의 로고"
+        />
+      </div>
+      <div className="StoreMenuText" style={{ float: "left", margin: 10 }}>
+        <b>종점떡볶이</b>
+        <br></br>
+        <AiFillStar />
+        {"     "}4.9 (102) <br></br>
+        최소 주문 금액 <b>15,000원</b>
+        <br></br>
+        배달 소요 시간 <b>23분</b>
+      </div>
+      <div style={{ clear: "both" }}></div>
+
+      <hr style={{ width: 500 }}></hr>
+      <div className="StoreInfoImage" style={{ float: "left", margin: 10 }}>
+        <img
+          onClick={() => {
+            window.location.href = `/storelist/${id}`;
+            // navigate(`/storelist/${id}`);
+            // onClick={() => (window.location.hrf = `/storelist/${id}`)}
+          }}
+          className="storeImg"
+          src="./img/storelogo_김밥천국-명동본점.png"
+          width="100"
+          height="100"
+          alt="김밥천국 명동본점의 로고"
+        />
+      </div>
+      <div className="StoreMenuText" style={{ float: "left", margin: 10 }}>
+        <b>김밥천국-명동본점</b>
+        <br></br>
+        <AiFillStar />
+        {"     "}4.7 (957) <br></br>
+        최소 주문 금액 <b>14,000원</b>
+        <br></br>
+        배달 소요 시간 <b>54~64분</b>
+      </div>
+      <div style={{ clear: "both" }}></div>
+      <hr style={{ width: 500 }}></hr>
+      <div className="StoreInfoImage" style={{ float: "left", margin: 10 }}>
+        <img
+          onClick={() => {
+            window.location.href = `/storelist/${id}`;
+            // navigate(`/storelist/${id}`);
+            // onClick={() => (window.location.hrf = `/storelist/${id}`)}
+          }}
+          className="storeImg"
+          src="./img/storelogo_마포만두-시청점.png"
+          width="100"
+          height="100"
+          alt="마포만두 시청점의 로고"
+        />
+      </div>
+      <div className="StoreMenuText" style={{ float: "left", margin: 10 }}>
+        <b>마포만두-시청점</b>
+        <br></br>
+        <AiFillStar />
+        {"     "}4.7 (862) <br></br>
+        최소 주문 금액 <b>15,000원</b>
+        <br></br>
+        배달 소요 시간 <b>35분</b>
+      </div>
+      <div style={{ clear: "both" }}></div>
+
+      <hr style={{ width: 500 }}></hr>
+      <div className="StoreInfoImage" style={{ float: "left", margin: 10 }}>
+        <img
+          onClick={() => {
+            window.location.href = `/storelist/${id}`;
+            // navigate(`/storelist/${id}`);
+            // onClick={() => (window.location.hrf = `/storelist/${id}`)}
+          }}
+          className="storeImg"
+          src="./img/storelogo_청담동마녀김밥-상왕십리점.png"
+          width="100"
+          height="100"
+          alt="청담동마녀김밥 상왕십리점의 로고"
+        />
+      </div>
+      <div className="StoreMenuText" style={{ float: "left", margin: 10 }}>
+        <b>청담동마녀김밥-상왕십리점</b>
+        <br></br>
+        <AiFillStar />
+        {"     "}4.9 (307) <br></br>
+        최소 주문 금액 <b>23,000원</b>
+        <br></br>
+        배달 소요 시간 <b>35분</b>
+      </div>
+      <div style={{ clear: "both" }}></div>
+
+      <hr style={{ width: 500 }}></hr>
+      <div className="StoreInfoImage" style={{ float: "left", margin: 10 }}>
+        <img
+          onClick={() => {
+            window.location.href = `/storelist/${id}`;
+            // navigate(`/storelist/${id}`);
+            // onClick={() => (window.location.hrf = `/storelist/${id}`)}
+          }}
+          className="storeImg"
+          src="./img/storelogo_찐핵볶이.png"
+          width="100"
+          height="100"
+          alt="찐핵볶이의 로고"
+        />
+      </div>
+      <div className="StoreMenuText" style={{ float: "left", margin: 10 }}>
+        <b>찐핵볶이</b>
+        <br></br>
+        <AiFillStar />
+        {"     "}4.9 (58) <br></br>
+        최소 주문 금액 <b>12,000원</b>
+        <br></br>
+        배달 소요 시간 <b>72~82분</b>
+      </div>
+      <div style={{ clear: "both" }}></div>
     </>
   );
 };
